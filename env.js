fc90e3f63bb44973a0ea47a8be788297ca280c28
Soul244 <hasancangedik@hotmail.com>
2019-02-04 15:18:10 +0300

    (fix) apiUrl changed to local

--- env.js
+++ env.js
@@ -1,5 +1,5 @@
 
 module.exports = {
-  'process.env.apiUrl': 'https://node-js-230621.appspot.com/',
+  'process.env.apiUrl': 'http//localhost:3000/',
   'process.env.localhost': 'http//localhost:3000/',
 };
