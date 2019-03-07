1cc757471212c930ba4bbb4d805cd220fcf2fca3
Soul244 <hasancangedik@hotmail.com>
2019-03-08 02:02:22 +0300

    (style) some styling

--- containers/Home/index.jsx
+++ containers/Home/index.jsx
@@ -6,18 +6,20 @@ import Link from 'next/link';
 import Parallax from 'react-springy-parallax';
 import styled, { keyframes } from 'styled-components';
 import {
-  Container, Row, Col, Button,
+  Container,
+  Row,
+  Col,
+  Button,
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
 } from 'reactstrap';
-import Typed from 'react-typed';
 
 import axios from '../../redux/axios';
-import Icon, {
-  arrowDown,
-} from '../../css/icons';
-
-const ContainerStyled = styled(Container)`
-  height: 100%;
-`;
+import Icon, { arrowDown } from '../../css/icons';
 
 const RowStyled = styled(Row)`
   justify-content: center;
@@ -28,27 +30,50 @@ const RowStyled = styled(Row)`
 const ContainerBackground = styled.div`
   background-image: url(${props => props.image});
   height: 100%;
+  background-attachment: fixed;
   background-position: center;
   background-repeat: no-repeat;
   background-size: cover;
   align-items: center;
   justify-content: center;
-  background-color: ${props => (props.bg ? '#F0F0F0' : '#fff')};
+  height: ${props => props.height};
+  background-color: ${props => (props.bg ? '#F8F8F8' : '#fff')};
+  padding: 8rem 0;
+  position:relative;
+  ::before{
+    content:'';
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top:0;
+    background-color: rgba(${props => (props.image ? '0, 0, 0,0.1' : '')});
+  }
+  h1,
+  h2 {
+    color: #17a2b8;
+  }
+  p{
+    color: black;
+  }
 `;
 
-const HeaderContainer = styled.div`
-  background: linear-gradient(to bottom,rgba(10,10,10,0.7) 75%,transparent);
-  border-radius: 50px;
-  padding: 1rem 2rem;
+const List = styled.ul`
   text-align: center;
 `;
 
-const Header = styled.h1`
-  color: white;
-`;
-
-const Text = styled.p`
-  color: white;
+const Title = styled.h1`
+position: relative;
+margin-bottom:16px;
+  ::before{
+    content: '';
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 8px;
+    left:0;
+    bottom: 0;
+    border-bottom: 1px solid #17a2b8;
+  }
 `;
 
 const iconAnimation = keyframes`
@@ -62,29 +87,22 @@ const IconContainer = styled.div`
   position: absolute;
   bottom: 20px;
   animation: ${iconAnimation} 2s linear infinite;
-  svg{
+  svg {
     fill: white;
-  }
-`;
-
-const ButtonList = styled.div`
-  display: flex;
-  flex-direction: column;
-  button{
-    margin-top: 2rem;
   }
 `;
 
 const TextContainer = styled.div`
   display: flex;
   flex-direction: column;
-  text-align:center;
+  text-align: center;
 `;
 
 const Image = styled.img`
     height: ${props => props.height};
     width: auto;
 `;
+
 
 const Footer = styled.div`
   display: flex;
@@ -99,9 +117,10 @@ const Footer = styled.div`
 
 class index extends Component {
   componentDidMount() {
-    axios.post('/users/token', {
-      token: localStorage.getItem('token'),
-    })
+    axios
+      .post('/users/token', {
+        token: localStorage.getItem('token'),
+      })
       .then((response) => {
         const { isTokenValid } = response.data;
         if (!isTokenValid) {
@@ -118,117 +137,145 @@ class index extends Component {
   render() {
     return (
       <Container fluid style={{ padding: 0 }}>
-        <Parallax ref="parallax" pages={4}>
-          <Parallax.Layer
-            offset={0}
-            speed={0}
-          >
-            <ContainerBackground image="/static/image_1.jpeg">
-              <ContainerStyled>
-                <RowStyled>
-                  <Col>
-                    <HeaderContainer>
-                      <Header>Ücretsiz anketlerinizi oluşturun ve paylaşın.</Header>
-                      <Text>
-                      Anketlerinizi oluşturun, paylaşın ve istatistiklerinizi görün. Tamamen ücretsiz!
-                      </Text>
-                      <Link as="/giris-yap" href="/auth">
-                        <Button type="button" color="info">
-                        Ücretsiz hesap
-                        </Button>
-                      </Link>
-                    </HeaderContainer>
-                  </Col>
-                  <IconContainer>
-                    <Icon size="48px" icon={arrowDown} onClick={() => this.refs.parallax.scrollTo(1)} />
-                  </IconContainer>
-                </RowStyled>
-              </ContainerStyled>
-            </ContainerBackground>
-          </Parallax.Layer>
-          <Parallax.Layer
-            offset={1}
-            speed={0}
-            onClick={() => this.refs.parallax.scrollTo(2)}
-          >
-            <ContainerBackground>
-              <ContainerStyled>
-                <RowStyled>
-                  <Col md="6">
-                    <TextContainer>
-                      <h2>Mobil</h2>
-                      <Typed
-                        strings={['Basit ve mobil için uyumlu.']}
-                        typeSpeed={40}
-                        backSpeed={50}
-                        loop
-                      />
-                    </TextContainer>
-                  </Col>
-                  <Col md="6">
-                    <Image src="/static/phone.png" height="700px" alt="" />
-                  </Col>
-                </RowStyled>
-              </ContainerStyled>
-            </ContainerBackground>
-          </Parallax.Layer>
-          <Parallax.Layer
-            offset={2}
-            speed={0}
-            onClick={() => this.refs.parallax.scrollTo(0)}
-          >
-            <ContainerBackground bg>
-              <ContainerStyled>
-                <RowStyled>
-                  <Col md="6">
-                    <Image src="/static/image_2.png" height="400px" alt="" />
-                  </Col>
-                  <Col md="6">
-                    <TextContainer>
-                      <h2>İstatistik</h2>
-                      <Typed
-                        strings={['Detaylı istatistiklerini görün ve dışarı aktarın.']}
-                        typeSpeed={40}
-                        backSpeed={50}
-                        loop
-                      />
-                    </TextContainer>
-                  </Col>
-                </RowStyled>
-              </ContainerStyled>
-            </ContainerBackground>
-          </Parallax.Layer>
-          <Parallax.Layer
-            offset={3}
-            speed={0}
-            onClick={() => this.refs.parallax.scrollTo(0)}
-          >
-            <ContainerBackground>
-              <ContainerStyled>
-                <RowStyled>
-                  <Col md="6">
-                    <TextContainer>
-                      <h2>Örnekler</h2>
-                      <p>Örnek testlere göz atın.</p>
-                    </TextContainer>
-                  </Col>
-                  <Col md="6">
-                    <ButtonList>
-                      <Button color="secondary">Yerel Seçim</Button>
-                      <Button color="secondary">Kim kazanır?</Button>
-                      <Button color="secondary" type="button">Araba tercihleri</Button>
-                    </ButtonList>
-                  </Col>
-                </RowStyled>
-              </ContainerStyled>
-            </ContainerBackground>
-            <Footer>
-              <a href="/about" as="/hakkimizda">Biz kimiz?</a>
-            </Footer>
-          </Parallax.Layer>
-        </Parallax>
+        <ContainerBackground image="/static/image_3.jpg">
+          <Container>
+            <RowStyled>
+              <Col>
+                <Title>Ücretsiz anketlerinizi oluşturun ve paylaşın.</Title>
+                <p>
+                  Anketlerinizi oluşturun, paylaşın ve istatistiklerinizi görün.
+                  Tamamen ücretsiz!
+                </p>
+                <Link as="/giris-yap" href="/auth">
+                  <Button type="button" color="info">
+                    Ücretsiz hesap
+                  </Button>
+                </Link>
+                {' '}
+                <Link as="/giris-yap" href="/auth">
+                  <Button type="button" color="secondary">
+                    Giriş yapın
+                  </Button>
+                </Link>
+              </Col>
+              <IconContainer>
+                <Icon
+                  size="48px"
+                  icon={arrowDown}
+                />
+              </IconContainer>
+            </RowStyled>
+          </Container>
+        </ContainerBackground>
+        <ContainerBackground>
+          <Container>
+            <RowStyled>
+              <Col md="12">
+                <h2>Örnek Anketler</h2>
+              </Col>
+              <Col md="4">
+                <Card>
+                  <CardImg
+                    top
+                    width="100%"
+                    src="/static/image_4.jpeg"
+                    alt="Card image cap"
+                  />
+                  <CardBody>
+                    <CardTitle>Maçın Favorisi Kim?</CardTitle>
+                    <CardSubtitle>Lig maçının favorisini seçin.</CardSubtitle>
+                    <CardText>
+                      Fenerbahçe ile Beşiktaş arasında oynanacak dev derbiyi kim
+                      kazanacak? Ankete katıl ve çekilişe katıl.
+                    </CardText>
+                    <Button outline>Ankete Git</Button>
+                  </CardBody>
+                </Card>
+              </Col>
+              <Col md="4">
+                <Card>
+                  <CardImg
+                    top
+                    width="100%"
+                    src="/static/image_5.jpeg"
+                    alt="Card image cap"
+                  />
+                  <CardBody>
+                    <CardTitle>En İyi Araba Lastiği Hangisi?</CardTitle>
+                    <CardSubtitle>
+                      Sizin için en iyi araba lastiğini seçin.
+                    </CardSubtitle>
+                    <CardText>
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </CardText>
+                    <Button outline>Ankete Git</Button>
+                  </CardBody>
+                </Card>
+              </Col>
+              <Col md="4">
+                <Card>
+                  <CardImg
+                    top
+                    width="100%"
+                    src="/static/image_6.jpeg"
+                    alt="Card image cap"
+                  />
+                  <CardBody>
+                    <CardTitle>
+                      Yerel Seçimlerde Kime Oy Vereceksiniz?
+                    </CardTitle>
+                    <CardSubtitle>Card subtitle</CardSubtitle>
+                    <CardText>
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </CardText>
+                    <Button outline>Ankete Git</Button>
+                  </CardBody>
+                </Card>
+              </Col>
+            </RowStyled>
+          </Container>
+        </ContainerBackground>
+        <ContainerBackground bg>
+          <Container>
+            <RowStyled>
+              <Col md="6">
+                <TextContainer>
+                  <h2>Mobil</h2>
+                  <List>
+                    <li>Aliqua anim tempor esse laboris velit.</li>
+                    <li>Est Lorem cillum tempor ut enim ipsum.</li>
+                    <li>Minim esse commodo sint ullamco ullamco nostrud est cillum labore id quis laboris occaecat.</li>
+                  </List>
+                </TextContainer>
+              </Col>
+              <Col md="6" className="text-center">
+                <Image src="/static/phone.png" height="500px" alt="" />
+              </Col>
+            </RowStyled>
+          </Container>
+        </ContainerBackground>
+        <ContainerBackground>
+          <Container>
+            <RowStyled>
+              <Col md="6" className="text-center">
+                <Image src="/static/image_2.png" height="300px" alt="" />
+              </Col>
+              <Col md="6">
+                <TextContainer>
+                  <h2>İstatistik</h2>
+                  <ul>
+                    <li>Aliqua anim tempor esse laboris velit.</li>
+                    <li>Est Lorem cillum tempor ut enim ipsum.</li>
+                    <li>Minim esse commodo sint ullamco ullamco nostrud est cillum labore id quis laboris occaecat.</li>
+                  </ul>
+                </TextContainer>
+              </Col>
+            </RowStyled>
+          </Container>
+        </ContainerBackground>
       </Container>
-
     );
   }
 }
