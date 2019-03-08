cf66e5d8b3af779fc7b08fc06dab42d257d7f757
Soul244 <hasancangedik@hotmail.com>
2019-03-08 15:49:11 +0300

    (style) some styling works

---
+++ components/UserPolls/Actions.jsx
@@ -0,0 +1,122 @@
+import React from 'react';
+import Link from 'next/link';
+import PropTypes from 'prop-types';
+
+import {
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  ButtonDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+} from 'reactstrap';
+import styled from 'styled-components';
+
+import Icon, {
+  remove2, edit, stats, iframe, showPoll,
+} from '../../css/icons';
+import Iframe from './Iframe';
+
+const LinkText = styled.p`
+    display:inline-block;
+    margin:0 0 0 0.5rem;
+    font-size: 16px;
+`;
+
+class Actions extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modal: false,
+      dropdownOpen: false,
+      showDelete: false,
+    };
+  }
+
+  toggle = () => {
+    this.setState(prevState => ({
+      modal: !prevState.modal,
+    }));
+  }
+
+  dropdownToggle= () => {
+    this.setState(prevState => ({
+      dropdownOpen: !prevState.dropdownOpen,
+    }));
+  }
+
+  toggleDeleteModal = () => {
+    this.setState(prevState => ({
+      showDelete: !prevState.showDelete,
+    }));
+  }
+
+  onClick = (_id) => {
+    const { deletePoll } = this.props;
+    deletePoll(_id);
+    this.toggleDeleteModal();
+  }
+
+  render() {
+    const { _id, slug } = this.props;
+    const {
+      dropdownOpen, modal, showDelete,
+    } = this.state;
+    return (
+      <>
+        <ButtonDropdown isOpen={dropdownOpen} toggle={this.dropdownToggle} direction="down">
+          <DropdownToggle caret>
+              Aksiyon
+          </DropdownToggle>
+          <DropdownMenu>
+            <Link as={`/anket/${slug}`} href={`/poll?slug=${slug}`}>
+              <DropdownItem id={`embed${_id}`}>
+                <a target="_blank">
+                  <Icon size="16px" icon={showPoll} />
+                  <LinkText>Ankete git</LinkText>
+                </a>
+              </DropdownItem>
+            </Link>
+            <Link as={`/anket/editor/${slug}`} href={`/poll/editor?slug=${slug}`}>
+              <DropdownItem id={`update${_id}`}>
+                <Icon size="16px" icon={edit} />
+                <LinkText>Güncelle</LinkText>
+              </DropdownItem>
+            </Link>
+            <DropdownItem onClick={this.toggleDeleteModal} id={`delete${_id}`}>
+              <Icon size="16px" icon={remove2} />
+              <LinkText>Sil</LinkText>
+            </DropdownItem>
+            <Link as={`/anket/istatistikler/${slug}`} href={`/poll/stats?slug=${slug}`}>
+              <DropdownItem>
+                <Icon size="16px" icon={stats} />
+                <LinkText>İstatistikler</LinkText>
+              </DropdownItem>
+            </Link>
+            <DropdownItem onClick={this.toggle} id={`iframe${_id}`}>
+              <Icon size="16px" icon={iframe} />
+              <LinkText>Iframe kodu</LinkText>
+            </DropdownItem>
+          </DropdownMenu>
+        </ButtonDropdown>
+        <Iframe modal={modal} pollName={slug} toggle={this.toggle} />
+        <Modal isOpen={showDelete} toggle={this.toggleDeleteModal}>
+          <ModalHeader toggle={this.toggleDeleteModal}>Uyarı</ModalHeader>
+          <ModalBody>
+              Anketi silmek istediğinize emin misiniz?
+          </ModalBody>
+          <ModalFooter>
+            <Button color="danger" onClick={() => this.onClick(_id)}>Sil</Button>
+            {' '}
+            <Button color="secondary" onClick={this.toggleDeleteModal}>Hayır</Button>
+          </ModalFooter>
+        </Modal>
+      </>
+    );
+  }
+}
+
+export default Actions;
--- components/UserPolls/PollList.jsx
+++ components/UserPolls/PollList.jsx
@@ -1,54 +1,28 @@
 import React from 'react';
-import Link from 'next/link';
 import moment from 'moment';
 import PropTypes from 'prop-types';
 
 import {
   Table,
-  Button,
-  Modal,
-  ModalHeader,
-  ModalBody,
-  ModalFooter,
-  UncontrolledTooltip,
 } from 'reactstrap';
 import styled from 'styled-components';
 import { ToastContainer } from 'react-toastify';
-import Icon, {
-  remove2, edit, stats, iframe, showPoll,
-} from '../../css/icons';
 
-import Iframe from './Iframe';
-
-const ButtonList = styled.div`
-  display:flex;
-  flex-direction:row;
-  @media (max-width: 992px) {
-    flex-direction:column;
-    }
-`;
-
-const ButtonStyled = styled(Button)`
-  margin: 0 4px;
-  width: 50px;
-  @media (max-width: 992px) {
-    margin: 4px 0;
-    }
-`;
+import Actions from './Actions';
 
 const TableStyled = styled(Table)` 
   background-color: #fff;
+  tr:hover{
+    color: #009fd4;
+    box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+
+  }
+  td:hover{
+    color: #009fd4;
+  }
 `;
 
 class PollList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      modal: false,
-      showDelete: false,
-    };
-  }
-
   notify = (message) => {
     /*
         if (message) {
@@ -57,30 +31,14 @@ class PollList extends React.Component {
     */
   };
 
-  toggle = () => {
-    this.setState(prevState => ({
-      modal: !prevState.modal,
-    }));
-  }
-
-  toggleDelete = () => {
-    this.setState(prevState => ({
-      showDelete: !prevState.showDelete,
-    }));
-  }
-
-  removePoll = (id) => {
-    this.props.deletePoll(id);
-    this.toggleDelete();
-  }
 
   render() {
-    const { polls, message } = this.props;
+    const { polls, message, deletePoll } = this.props;
     return (
       <>
         {this.notify(message)}
         <ToastContainer autoClose={2000} />
-        <TableStyled bordered hover responsive>
+        <TableStyled hover responsive>
           <thead>
             <tr>
               <th>Anket Başlığı</th>
@@ -97,46 +55,7 @@ class PollList extends React.Component {
                   <td>{item.slug}</td>
                   <td>{moment(item.createdAt).format('DD-MM-YYYY')}</td>
                   <td>
-                    <ButtonList>
-                      <Link as={`/anket/${item.slug}`} href={`/poll?slug=${item.slug}`}>
-                        <a target="_blank">
-                          <ButtonStyled outline color="success" id={`embed${item._id}`}>
-                            <Icon size="24px" icon={showPoll} />
-                          </ButtonStyled>
-                        </a>
-                      </Link>
-                      <Link as={`/anket/editor/${item.slug}`} href={`/poll/editor?slug=${item.slug}`}>
-                        <ButtonStyled outline color="info" id={`update${item._id}`}>
-                          <Icon size="24px" icon={edit} />
-                        </ButtonStyled>
-                      </Link>
-                      <ButtonStyled outline color="danger" onClick={this.toggleDelete} id={`delete${item._id}`}>
-                        <Icon size="24px" icon={remove2} />
-                      </ButtonStyled>
-                      <Modal isOpen={this.state.showDelete} toggle={this.toggleDelete}>
-                        <ModalHeader toggle={this.toggleDelete}>Uyarı</ModalHeader>
-                        <ModalBody>Anketi silmek istediğinize emin misiniz?</ModalBody>
-                        <ModalFooter>
-                          <Button color="danger" onClick={() => this.removePoll(item._id)}>Sil</Button>
-                          {' '}
-                          <Button color="secondary" onClick={this.toggleDelete}>Hayır</Button>
-                        </ModalFooter>
-                      </Modal>
-                      <Link as={`/anket/istatistikler/${item.slug}`} href={`/poll/stats?slug=${item.slug}`}>
-                        <ButtonStyled outline color="secondary" id={`stats${item._id}`}>
-                          <Icon size="24px" icon={stats} />
-                        </ButtonStyled>
-                      </Link>
-                      <ButtonStyled outline color="secondary" onClick={this.toggle} id={`iframe${item._id}`}>
-                        <Icon size="24px" icon={iframe} />
-                      </ButtonStyled>
-                      <Iframe modal={this.state.modal} pollName={item.slug} toggle={this.toggle} />
-                    </ButtonList>
-                    <UncontrolledTooltip placement="bottom" target={`embed${item._id}`}>Ankete git</UncontrolledTooltip>
-                    <UncontrolledTooltip placement="bottom" target={`delete${item._id}`}>Sil</UncontrolledTooltip>
-                    <UncontrolledTooltip placement="bottom" target={`update${item._id}`}>Güncelle</UncontrolledTooltip>
-                    <UncontrolledTooltip placement="bottom" target={`stats${item._id}`}>İstatistikler</UncontrolledTooltip>
-                    <UncontrolledTooltip placement="bottom" target={`iframe${item._id}`}>IFrame kodunu al</UncontrolledTooltip>
+                    <Actions _id={item._id} slug={item.slug} deletePoll={deletePoll} />
                   </td>
                 </tr>
               ))
