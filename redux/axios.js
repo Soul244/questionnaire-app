7e9de38e2e46aae54464ba071d9a2ee2a67358b0
Soul244 <hasancangedik@hotmail.com>
2019-04-10 18:27:03 +0300

    (fix) baseUrl

--- redux/axios.js
+++ redux/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create();
 
-// instance.defaults.baseURL = 'https://questionnaire-test-api.herokuapp.com/';
-instance.defaults.baseURL = 'http://localhost:3001/';
+instance.defaults.baseURL = 'https://questionnaire-test-api.herokuapp.com/';
+// instance.defaults.baseURL = 'http://localhost:3001/';
 
 export default instance;
